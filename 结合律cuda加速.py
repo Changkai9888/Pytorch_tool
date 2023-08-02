15fd7ad615a19e5db471e720482d64ef62b4dbb9
Changkai9888 <141225131+Changkai9888@users.noreply.github.com>
2023-08-02 17:33:32 +0800

    矩阵序列的结合律的并行加速！

---
+++ 结合律cuda加速.py
@@ -0,0 +1,53 @@
+import torch,time,fc
+def f(a,b):
+    return a@b
+def associa(a,f):
+    "f为结合律双元算符"
+    n=int(len(a)**0.5)
+    i=0
+    sum_a=[]
+    while n*(i+1)<=len(a):
+        sum_a+=[a[n*i:n*(i+1)].unsqueeze(0)]
+        i+=1
+    size=[n*i]+list(a.shape)[1:]
+    sum_a=torch.cat(sum_a,dim=0)
+    result=torch.clone(a)
+    for i in range(1,len(sum_a[0])):
+        sum_a[:,i]=f(sum_a[:,i-1],sum_a[:,i])
+    for i in range(1,len(sum_a)):
+        sum_a[i,:]=f(sum_a[i-1,-1],sum_a[i,:])
+    result[:size[0]]=sum_a.reshape(size)
+    for i in range(n**2,len(a)):
+        result[i]=f(result[i-1],a[i])
+    return result
+
+tim1=[];tim2=[]
+for i in range(1):
+    a=torch.rand(200000,2,2).cuda()
+    tim=time.time()
+    result=associa(a,f)
+    tim1+=[time.time()-tim]
+
+    tim=time.time()
+    re=torch.clone(a)
+    re[0]=a[0]
+    for i in range(1,len(a)):
+        re[i]=re[i-1]@a[i]
+    tim2+=[time.time()-tim]
+for i in range(1):
+    a=torch.rand(200000,2,2)
+    tim=time.time()
+    result=associa(a,f)
+    tim1+=[time.time()-tim]
+
+    tim=time.time()
+    re=torch.clone(a)
+    re[0]=a[0]
+    for i in range(1,len(a)):
+        re[i]=re[i-1]@a[i]
+    tim2+=[time.time()-tim]
+
+result[-1]==re[-1]
+print(result[-1],re[-1])
+print(result[int(len(a)**0.5)+13],re[int(len(a)**0.5)+13])
+fc.plot([tim1,tim2],k=1)
