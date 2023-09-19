6a5cffdbf3fc6efcb5bed64c7c51630f7e6a2314
Changkai9888 <141225131+Changkai9888@users.noreply.github.com>
2023-09-19 21:32:50 +0800

    Update associative_law_operation_accelerator_for_pyorch.py
    1，整体做了优化加速，将batch的N做了更好的拆分，另计算速度总体提高了30%左右。（算法受“科学空间”社区群友启发得到）
    2，归一化做出了优化，令归一化更平滑，对于长序列几乎不会有明显的数值漂移。
    3，归一化新增了行归一化功能，完整化了行列的归一化功能。
    translating for English reader:
    1. The overall optimization and acceleration have been made, the N of the batch has been better split, and the overall calculation speed has been increased by about 30%. (The algorithm was inspired by members of the "Science Space" community)
    2. The normalization has been optimized to make the normalization smoother and there will be almost no obvious numerical drift for long sequences.
    3. Normalization adds a new row normalization function, which completes the normalization function of rows and columns.

--- associative_law_operation_accelerator_for_pyorch.py
+++ associative_law_operation_accelerator_for_pyorch.py
@@ -1,21 +1,30 @@
 import torch,time,fc
 from torch.cuda.amp import autocast
 def get_Normalization(a,dim):
-    "归一化：数据形状要求: batch*行 *列"
-    if dim==-1:#列归一化
-        sum_a=torch.sum(a,dim=1)
-        a=a/(sum_a.unsqueeze(1).expand(a.shape))
-        #print(sum_a)
+    '''归一化：数据形状要求: batch*行 *列；
+    dim=0，直接返回；dim=1，行归一；dim=-1，列归一'''
+    if dim!=0:
+        if dim==-1:#列归一化
+            '''if len(a.shape)==2:#单个矩阵,不是最后的输出，中途的归一化。
+                sum_a=torch.sum(a,dim=0)
+                a=a/sum_a
+            else:#batch序列'''
+            sum_a=torch.sum(a,dim=1)
+            a=a/(sum_a.unsqueeze(1).expand(a.shape))
+        elif dim==1:#行归一化
+            '''if len(a.shape)==2:#单个矩阵,不是最后的输出，中途的归一化。
+                sum_a=torch.sum(a,dim=1)
+                a=a/sum_a.unsqueeze(1)
+            else:'''
+            sum_a=torch.sum(a,dim=2)
+            a=a/sum_a.unsqueeze(2)
         if torch.isnan(sum_a).any() or torch.isinf(sum_a).any():#判断是否有Nan或者inf的坏点
-            print(max(sum_a[-1]),min(sum_a[-1]))
             print(sum_a.dtype)
             print(sum_a)
             fc.save_temp(sum_a,'sum_a')
-            fc.plot(sum_a[:,0])
+            if len(a.shape)>2:
+                fc.plot(sum_a[:,0])
             stop;
-        if __name__=='__main__':
-            print(sum_a)
-            fc.plot(sum_a[:,0])
     return a
 #f: 1), [n_old]*[n_new]    2), 1_old*[n_new]    3), 1_old*1_new
 #例如：选择穿透
@@ -29,75 +38,91 @@ def f_gather(old,new):#递归切片
         return torch.gather(new.unsqueeze(0), 1, old.unsqueeze(0)).squeeze(0)
 
 def associa(a,f=1,Normalization=0):
-    '''输入矩阵序列a=[A1,A2,A3……A(i)]，输出为a的累次结合律二元运算序列result=[A1,A1*A2,A1*A2*A3,……,A1*A2*A3*……A(i-1)*A(i)]
-    注意：f为结合律双元算符
+    '''输入序列a=[A1,A2,A3……A(i)]（比如一个矩阵序列），输出为a的累次结合律二元运算序列：
+    return=[A1,A1*A2,A1*A2*A3,……,A1*A2*A3*……A(i-1)*A(i)]；
+    上式中，*代表符合结合律的双元算符，参数f表示这个算符函数（特别的，在f=1时表示矩阵乘法）；
     Normalization：0，不归一化；-1，列归一化'''
-    #print(a.dtype)
+    #print(a)
     if f==1:
         f=lambda old,new: old@new
     elif f==-1:
         f=lambda old,new: new@old
-    n=int(len(a)**0.5)
-    i=0
-    sum_a=[]
-    while n*(i+1)<=len(a):
-        sum_a+=[a[n*i:n*(i+1)].unsqueeze(0)]
-        i+=1
-    size=[n*i]+list(a.shape)[1:]
-    sum_a=torch.cat(sum_a,dim=0)
+        
+    N=len(a);rec=N
+    for i in range(int((N/2)**0.5),int(N**0.5+1)):
+        _1,_2=int(N/i),i
+        _3=N-_1*_2+_1+_2
+        if _3<rec:
+            num1,num2,rec=_1,_2,_3
+    #print(num1,num2,num1*num2)
+    sum_a=a[:num1*num2].reshape([num1,num2]+list(a.shape)[1:])
     result=sum_a[:,0].unsqueeze(1)
     for i in range(1,len(sum_a[0])):#多行并行，横向复制
-        result=torch.cat((result,f(result[:,i-1],sum_a[:,i]).unsqueeze(1)),dim=1)
+        result=torch.cat((result,f(result[:,i-1],sum_a[:,i]).unsqueeze(1)),dim=1)#见下面，注释1
+    result=torch.cat( [result[:,:-1],get_Normalization(result[:,-1],dim=Normalization).unsqueeze(1)] ,dim=1)#过滤一下归一化问题
     result_1=result[0,:].unsqueeze(0)
     for i in range(1,len(sum_a)):#逐行，追尾复制
         result_1=torch.cat((result_1,f(result_1[i-1,-1],result[i,:]).unsqueeze(0)),dim=0)
-    sum_a=result_1.reshape(size)
-    for i in range(size[0],len(a)):
+    sum_a=result_1.reshape([num1*num2]+list(a.shape)[1:])
+    for i in range(num1*num2,len(a)):
         sum_a=torch.cat((sum_a,f(sum_a[i-1],a[i]).unsqueeze(0)),dim=0)
     sum_a=get_Normalization(sum_a,dim=Normalization)
+    #注释1：这么写梯度可以正常传播，可能是由于torch的某种机制，cat可以传播梯度，而其他某些操作不可以
     return sum_a
 
 #归一性测试
-if __name__=='__main__':
-    tim1=[];tim2=[]
+if  __name__=='__main__':
     for i in range(1):
-        a=torch.rand(200000,3,3).cuda().to(torch.float16)
-        a[:,1]=0
-        a[:,2]=1-a[:,0]#列归一
-        a[:,:,0]*=0.9
-        a[:,:,2]*=0.9
-        a[:,1,0]+=0.1
-        a[:,1,2]+=0.1
-        result=associa(a,Normalization=-1)
-#速度测试的例子
+        a=torch.rand(200256,3,3).cuda().to(torch.float32)
+        a[:,:,1]=0
+        a[:,:,2]=1-a[:,:,0]#行归一
+    result=associa(a,Normalization=1)
+    print(f'运行结果:  32f，并行 cuda，归一化\n{result[-1]}')#比较运行结果
+    print(f'中途结果{int(len(a)*0.5)}运行结果:   16f，并行 cuda，归一化\n{result[int(len(a)*0.5)+13]}')
+    a=a.to(torch.float16)
+    result=associa(a,Normalization=1)
+    print(f'运行结果:  16f，并行 cuda，归一化\n{result[-1]}')#比较运行结果
+    print(f'中途结果{int(len(a)*0.5)}运行结果:   32f，并行 cuda，归一化\n{result[int(len(a)*0.5)+13]}')
+
+#速度测试的例子:
+"""注意：对于cuda，测试速度前，确保调用运行一遍associa()，
+以获得cuda对函数的初始化，这会明显的影响计算速度。"""
 if __name__=='__main__':
-    tim1=[];tim2=[]
-    for i in range(1):
-        a=torch.rand(400,2,2).cuda()
+    k=200253
+    compare={};
+    _=associa(torch.rand(15,2,2).cuda())#初始化associa
+    for i in range(1):#cuda
+        a=torch.rand(k,3,3).cuda()#.to(torch.float16)
+        a[:,:,1]=0
+        a[:,:,2]=1-a[:,:,0]#行归一
+        ####
         tim=time.time()
-        result=associa(a)
-        tim1+=[time.time()-tim]
-        tim=time.time()
+        result=associa(a,Normalization=1)
+        compare['并行 cuda ']=time.time()-tim
         re=torch.clone(a)
         re[0]=a[0]
+        tim=time.time()
         for i in range(1,len(a)):
             re[i]=re[i-1]@a[i]
-        tim2+=[time.time()-tim]
-    for i in range(1):
-        a=torch.rand(400,2,2)
+        compare['串行 cuda ']=time.time()-tim
+    print(f'比较运行结果:  并行 cuda，串行 cuda \n{result[-1]} \n{re[-1]}')#比较运行结果
+    print(f'比较中途{int(len(a)*0.5)}运行结果:  并行 cuda，串行 cuda \n{result[int(len(a)*0.5)+13]}\n{re[int(len(a)*0.5)+13]}')#比较中途运行结果
+    for i in range(1):#cpu
+        a=torch.rand(k,3,3).cpu()#.to(torch.float32)
+        a[:,:,1]=0
+        a[:,:,2]=1-a[:,:,0]#行归一
+        ####
         tim=time.time()
-        result=associa(a)
-        tim1+=[time.time()-tim]
-        tim=time.time()
+        result=associa(a,Normalization=1)
+        compare['并行 cpu ']=time.time()-tim
         re=torch.clone(a)
         re[0]=a[0]
+        tim=time.time()
         for i in range(1,len(a)):
             re[i]=re[i-1]@a[i]
-        tim2+=[time.time()-tim]
+        compare['串行 cpu ']=time.time()-tim
 
-    result[-1]==re[-1]
-    print(result[-1],re[-1])#比较运行结果
-    print(result[int(len(a)**0.5)+13],re[int(len(a)**0.5)+13])
-    print([tim1,tim2])#比较时间
-
-
+    print(f'比较运行结果:  并行 cpu，串行 cpu \n{result[-1]}\n{re[-1]}')#比较运行结果
+    print(f'比较中途{int(len(a)*0.5)}运行结果:  并行 cpu，串行 cpu  \n{result[int(len(a)*0.5)+13]}\n{re[int(len(a)*0.5)+13]}')#比较中途运行结果
+    print(f'比较时间：\n{compare}')#比较时间
+#cpu/cuda, 16/32f, 归一化，串/并行，分割方法 -->时间，结果
